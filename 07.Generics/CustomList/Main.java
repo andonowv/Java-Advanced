85502e0bcadfe03249cf2c1556a00bd9a8debf0e
andonowv <81648825+andonowv@users.noreply.github.com>
2022-03-20 00:56:14 +0200

    Add files via upload

---
+++ 07.Generics/CustomList/Main.java
@@ -0,0 +1,55 @@
+package CustomList;
+
+import com.sun.source.doctree.SeeTree;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+        CustomList <String> customList = new CustomList<String>();
+        while (!command.equals("END")){
+            String [] tokens = command.split("\\s+");
+            String operation = tokens[0];
+            switch (operation){
+                case "Add":
+                    String el = tokens[1];
+                    customList.add(el);
+                    break;
+                case "Remove":
+                    int elToRe = Integer.parseInt(tokens[1]);
+                    customList.remove(elToRe);
+                    break;
+                case "Contains":
+                    String contains = tokens [1];
+                    System.out.println(customList.contains(contains));
+                    break;
+                case "Swap":
+                    int firstIndex = Integer.parseInt(tokens[1]);
+                    int secondIndex = Integer.parseInt(tokens[2]);
+                    customList.swap(firstIndex, secondIndex);
+                    break;
+                case "Greater":
+                    String greater = tokens [1];
+                    System.out.println(customList.countGreaterThan(greater));
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Print":
+                    for (String token : customList) {
+                        System.out.println(token);
+                    }
+                    break;
+
+            }
+
+            command = scanner.nextLine();
+        }
+    }
+}
